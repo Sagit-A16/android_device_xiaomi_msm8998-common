48daff16cd80f682bb3e88328153ba4aee54d29d
deny <77781846+deny2311@users.noreply.github.com>
2024-07-13 10:20:49 +0700

    msm8998-common: Sign with our keys

--- msm8998.mk
+++ msm8998.mk
@@ -407,3 +407,6 @@ PRODUCT_PACKAGES += \
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/msm8998-common/msm8998-common-vendor.mk)
+
+# Sign with our key
+$(call inherit-product, vendor/sagit/keys/keys.mk)
